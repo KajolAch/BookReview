3992b7780dbc532ef8436801cfb9ce5a899013d8
KajolAch <kacharya@stevens.edu>
2018-12-10 16:43:36 -0500

    call to google api books

---
+++ public/js/displayBook.js
@@ -0,0 +1,51 @@
+(function() {
+    console.log("in displayBook js");
+    const searchForm = document.getElementById("SearchForm");
+  
+    if (searchForm) {
+        searchForm.addEventListener("submit", event => {
+        event.preventDefault();
+  
+        try {
+  
+            const BookNameElement = document.getElementById("BookName");
+            const BookName = BookNameElement.value;
+
+            if(BookName){
+                if(!typeof(BookName)=="string"){
+                    throw "Phrase passed is not a String";
+                  }
+                  else{
+                      var title='';
+                      var author='';
+                      var img='';
+                      
+                     $.get("https://www.googleapis.com/books/v1/volumes?q="+ BookName,function(response){
+                        console.log(response);
+                        for(i=0;i<response.items.length;i++){
+                            console.log(response.items[i].volumeInfo.title);
+                           title=$('<h5>'+response.items[i].volumeInfo.title + '</h5>');
+                           author=$('<h5>'+response.items[i].volumeInfo.authors + '</h5>');
+                           img=$('<img src='+response.items[i].volumeInfo.imageLinks.smallThumbnail +'></img>');
+                          // let listItem=title;
+                           //$("#BookList").append(listItem);
+                           title.appendTo("#BookList");
+                           author.appendTo("#BookList");
+                           img.appendTo("#BookList");
+                        }  
+                     });
+                     
+                     
+                  }
+              }
+           // console.log(textElement.value);
+         
+        } catch (e) {
+          const message = typeof e === "string" ? e : e.message;
+          // errorTextElement.textContent = e;
+          // errorContainer.classList.remove("hidden");
+        }
+      });
+    }
+  })();
+  
--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,16 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("pages/search");
 });
-router.post("/",(req,res) => {
-  res.redirect("bookinfo");
-});
+// router.post("/",(req,res) => {
+//   console.log("Inside post");
+//   console.log(req.body);
+//   const SearchBook= req.body.BookName;
+//   console.log(SearchBook);
+//   if(SearchBook==""){
+//     throw 'Enter a book name';
+//   }
+ 
+//   res.redirect("bookinfo");
+
+// });
 module.exports = router;
